1811dc2148036be85c1d5428171282ba01e3799d
bhoffmanlab <bhoffmanlab@gmail.com>
2016-02-17 14:15:24 -0500

    Updated blob decay code

--- blob_decay.m
+++ blob_decay.m
@@ -1,10 +1,12 @@
 function blob_decay(fname,bimg,outname,varargin)
 
 folder = varargin{end};
+addpath(folder)
+addpath(fullfile(folder,'FA Images'))
 f = file_search(fname,folder);
 nimg = length(f);
 mask = single(imread(bimg));
-if nargin == 4
+if nargin == 5
     blbs = load(varargin{1});
     nblbs = length(blbs);
 else
@@ -15,18 +17,23 @@ end
 if nimg == 0
     disp('No files found')
 else
-    blbarr = zeros(nblbs,nimg+1);
+    blbarr = zeros(nimg+1,nblbs+1);
     for i = 1:nimg
         img = single(imread(f{i}));
         sz = size(img);
         crop = [round(0.0246*sz(1)) round(0.0246*sz(1)) round(0.9509*sz(1)) round(0.9509*sz(1))];
         img = imcrop(img,crop);
         for j = 1:nblbs
-            blbarr(j,1) = blbs(j);
-            blbarr(j,i+1) = mean(img(mask == blbs(j)));
+            blbarr(1,j+1) = blbs(j);
+            blbarr(i+1,1) = i;
+            blbarr(i+1,j+1) = mean(img(mask == blbs(j)));
         end
     end
 end
+
+% blb_titles = mat2cell(blbs
 save(fullfile(folder,outname),'blbarr','-ascii')
+rmpath(folder)
+rmpath(fullfile(folder,'FA Images'))
 
 end
--- run_blobdecay.m
+++ run_blobdecay.m
@@ -1,13 +1,24 @@
 folder = input('Enter folder containing images: ','s');
 blob_images = file_search('fa_\w+.TIF',folder);
 exp_name = input('Enter the experimental group: ','s');
-
 for i = 1:length(blob_images)
     outname = ['blbdecay_' blob_images{i}(8:end-4) '.txt'];
     blb_file = (file_search(['blbd_' exp_name '_' num2str(i) '.txt'],folder));
     if isempty(blb_file)
-        blob_decay([exp_name '_FRAP_\d+.TIF'],blob_images{i},outname,folder)
+        blob_decay([exp_name '_FRAP_' num2str(i,'%02d') '_t\d+.TIF'],blob_images{i},outname,folder)
     else
-        blob_decay([exp_name '_FRAP_\d+.TIF'],blob_images{i},outname,blb_file,folder)
+        blob_decay([exp_name '_FRAP_' num2str(i,'%02d') '_t\d+.TIF'],blob_images{i},outname,blb_file{1},folder)
     end
+    
+%     addpath(folder)
+%     figure; hold on; box on;
+%     data = load(outname);
+%     data(1,:) = [];
+%     for j = 2:size(data,2)
+%         plot(data(5:end,1),data(5:end,j))
+%     end
+%     title([exp_name ' ' num2str(i)])
+%     xlabel('Time')
+%     ylabel('Intensity')
+%     rmpath(folder)
 end
